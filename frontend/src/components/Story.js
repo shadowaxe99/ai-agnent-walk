8cc6fc9d5e03170685503096ceb0be73aecdfdc5
Michael Gruen <michael.gruen9@gmail.com>
2023-11-01 22:22:45 -0400

    frontend/src/components/Story.js - commit created by https://agenthub.dev/pipeline?run_id=6yfpNjUcfC4aKEAHJRngyT

--- frontend/src/components/Story.js
+++ frontend/src/components/Story.js
@@ -1,55 +1,44 @@
 import React from 'react';
 
-class Story extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentSection: 0,
-            sections: [
-                {
-                    title: 'Introduction to AI Agents',
-                    content: 'AI agents are systems that perceive their environment and take actions to achieve their goals. They can be as simple as a thermostat or as complex as a self-driving car.'
-                },
-                {
-                    title: 'History of AI Agents',
-                    content: 'The concept of AI agents dates back to the early days of computer science. Alan Turing, often considered the father of AI, proposed the idea of machines that could mimic human intelligence.'
-                },
-                {
-                    title: 'Working Mechanism of AI Agents',
-                    content: 'AI agents work by perceiving their environment through sensors, processing the information, and then acting on it. The processing step often involves machine learning algorithms.'
-                },
-                {
-                    title: 'Real-world Applications of AI Agents',
-                    content: 'AI agents are used in a wide range of applications, from recommendation systems on e-commerce websites to autonomous vehicles, voice assistants, and more.'
-                }
-            ]
-        };
-    }
+class SketchPad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawing: false,
+      coordinates: []
+    };
+  }
 
-    nextSection = () => {
-        if (this.state.currentSection < this.state.sections.length - 1) {
-            this.setState({ currentSection: this.state.currentSection + 1 });
-        }
-    }
+  handleMouseDown = (event) => {
+    this.setState({
+      drawing: true,
+      coordinates: [...this.state.coordinates, { x: event.clientX, y: event.clientY }]
+    });
+  }
 
-    prevSection = () => {
-        if (this.state.currentSection > 0) {
-            this.setState({ currentSection: this.state.currentSection - 1 });
-        }
+  handleMouseMove = (event) => {
+    if (this.state.drawing) {
+      this.setState({
+        coordinates: [...this.state.coordinates, { x: event.clientX, y: event.clientY }]
+      });
     }
+  }
 
-    render() {
-        const { title, content } = this.state.sections[this.state.currentSection];
+  handleMouseUp = () => {
+    this.setState({
+      drawing: false
+    });
+  }
 
-        return (
-            <div id="explorationContainer">
-                <h2>{title}</h2>
-                <p>{content}</p>
-                <button onClick={this.prevSection} disabled={this.state.currentSection === 0}>Previous</button>
-                <button onClick={this.nextSection} disabled={this.state.currentSection === this.state.sections.length - 1}>Next</button>
-            </div>
-        );
-    }
+  render() {
+    const { drawing, coordinates } = this.state;
+
+    return (
+      <div id="sketchPad" onMouseDown={this.handleMouseDown} onMouseMove={this.handleMouseMove} onMouseUp={this.handleMouseUp}>
+        {drawing && <div className="brush" style={{ top: coordinates[coordinates.length - 1].y, left: coordinates[coordinates.length - 1].x }}></div>}
+      </div>
+    );
+  }
 }
 
-export default Story;
+export default SketchPad;
