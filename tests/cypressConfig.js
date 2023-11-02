77a67d98d30f9e5818d8f235fa8cb695377386f5
Michael Gruen <michael.gruen9@gmail.com>
2023-11-01 22:22:51 -0400

    tests/cypressConfig.js - commit created by https://agenthub.dev/pipeline?run_id=6yfpNjUcfC4aKEAHJRngyT

--- tests/cypressConfig.js
+++ tests/cypressConfig.js
@@ -1,20 +1,69 @@
-module.exports = {
-  integrationFolder: 'tests',
-  testFiles: '**/*Test.js',
-  video: true,
-  screenshotsFolder: 'tests/screenshots',
-  videosFolder: 'tests/videos',
-  viewportWidth: 1280,
-  viewportHeight: 720,
-  chromeWebSecurity: false,
-  defaultCommandTimeout: 10000,
-  execTimeout: 20000,
-  pageLoadTimeout: 30000,
-  waitForAnimations: true,
-  animationDistanceThreshold: 14,
-  baseUrl: 'http://localhost:3000',
-  env: {
-    username: 'testUser',
-    password: 'testPassword'
+const sketchPad = {
+  canvas: null,
+  context: null,
+  isDrawing: false,
+  lastX: 0,
+  lastY: 0,
+  colors: [],
+  code: "",
+  
+  init: function() {
+    this.canvas = document.querySelector("#sketch-pad");
+    this.context = this.canvas.getContext("2d");
+    this.canvas.addEventListener("mousedown", this.startDrawing.bind(this));
+    this.canvas.addEventListener("mousemove", this.draw.bind(this));
+    this.canvas.addEventListener("mouseup", this.stopDrawing.bind(this));
+    this.canvas.addEventListener("mouseout", this.stopDrawing.bind(this));
+  },
+  
+  startDrawing: function(e) {
+    this.isDrawing = true;
+    this.lastX = e.offsetX;
+    this.lastY = e.offsetY;
+    this.colors = [];
+    this.code = "";
+  },
+  
+  draw: function(e) {
+    if (!this.isDrawing) return;
+    const x = e.offsetX;
+    const y = e.offsetY;
+    this.drawOnCanvas(x, y);
+    this.buildCode(x, y);
+  },
+  
+  stopDrawing: function() {
+    this.isDrawing = false;
+    console.log(this.code); // Print the generated code
+  },
+  
+  drawOnCanvas: function(x, y) {
+    this.context.beginPath();
+    this.context.moveTo(this.lastX, this.lastY);
+    this.context.lineTo(x, y);
+    this.context.stroke();
+    this.lastX = x;
+    this.lastY = y;
+  },
+  
+  buildCode: function(x, y) {
+    const color = this.getColor(x, y);
+    if (color) {
+      if (!this.colors.includes(color)) {
+        this.colors.push(color);
+        this.code += `<div style="background-color: ${color}; width: 100px; height: 100px;"></div>\n`;
+      }
+    }
+  },
+  
+  getColor: function(x, y) {
+    const imageData = this.context.getImageData(x, y, 1, 1).data;
+    if (imageData[3] === 0) return null;
+    const [r, g, b] = imageData;
+    return `rgb(${r}, ${g}, ${b})`;
   }
+};
+
+window.onload = function() {
+  sketchPad.init();
 };
